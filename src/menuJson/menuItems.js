a17758b65210905a96f6d3b0eff57ada51481bd8
kalacs <divixed12@gmail.com>
2021-04-27 12:45:00 +0200

    Add Menu object

---
+++ src/menuJson/menuItems.js
@@ -0,0 +1,19 @@
+//This list contains objects with the Menus title: which will be shown in the header 
+//and the linkTo: which contains the route to the component and will be used in the LinkTo elem.
+
+export const mMenus = [{
+        title: "Products",
+        linkTo: "/products"
+    },
+    {
+        title: "Galery",
+        linkTo: "/galery"
+    },
+    {
+        title: "About Us",
+        linkTo: "/aboutus"
+    },
+    {
+        title: "Main Page",
+        linkTo: "mainpage"
+    }]

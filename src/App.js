2365a88e87caa8c0b3619913fb6905a9816fb251
kalacs <divixed12@gmail.com>
2021-04-27 12:47:24 +0200

    Add navbar to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Header from './components/Header';
+import {makeStyles} from '@material-ui/core/styles';
+
 
 function App() {
+
+  const classes = useStyles();
   return (
     <div>
-     
+      <Header/>
+      <section className={classes.section}/>
+      <section className={classes.section}/>
+      <section className={classes.section}/>
     </div>
   );
 }
 
 export default App;
+
+const useStyles = makeStyles({
+  section: {
+      height: '100vw',
+      width: '100%'
+  }})
